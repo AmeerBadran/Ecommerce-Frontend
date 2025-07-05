6c63fb9824fe182aadcb4e98df1b058a5665c65c
AmeerBadran <abadran281@gmail.com>
2025-07-06 00:51:34 +0300

    last update after start in admin pages orders customers products dashboar

--- constants/products.ts
+++ constants/products.ts
@@ -2,7 +2,8 @@ export const productsData = [
   {
     id: "1",
     title: "Plastic Dining Armchair",
-    image: "/images/willdelete2/product-14-13_788x101355.png",
+    image:
+      "https://res.cloudinary.com/df1oiiaat/image/upload/v1751751988/product-14-13_788x101366_ef2dvd.webp",
     price: 200.0,
     colors: ["#007F7F", "#000000", "#FFFF00"],
     offer: 0.4,
@@ -12,7 +13,8 @@ export const productsData = [
   {
     id: "2",
     title: "Modern White Chair",
-    image: "/images/willdelete2/product-14-13_788x1013.png",
+    image:
+      "https://res.cloudinary.com/df1oiiaat/image/upload/v1751751982/product-14-13_788x1013_fzgju8.webp",
     price: 260.0,
     colors: [],
     offer: 0.07,
@@ -22,7 +24,8 @@ export const productsData = [
   {
     id: "3",
     title: "Red Sacrificial Chair",
-    image: "/images/willdelete2/product-14-13_788x101311.png",
+    image:
+      "https://res.cloudinary.com/df1oiiaat/image/upload/v1751751984/product-14-13_788x101311_tp4j0p.webp",
     price: 170.0,
     colors: ["#800000", "#000000", "#FFFFFF"],
     offer: 0.2,
@@ -32,7 +35,8 @@ export const productsData = [
   {
     id: "4",
     title: "Gray Lounge Chair",
-    image: "/images/willdelete2/product-14-13_788x101322.png",
+    image:
+      "https://res.cloudinary.com/df1oiiaat/image/upload/v1751751985/product-14-13_788x101322_mhqdv7.webp",
     price: 180.0,
     colors: [],
     offer: 0.1,
@@ -42,7 +46,8 @@ export const productsData = [
   {
     id: "5",
     title: "Dark Gray Dining Chair",
-    image: "/images/willdelete2/11_700x900.png",
+    image:
+      "https://res.cloudinary.com/df1oiiaat/image/upload/v1751751981/11_700x900_j8nins.webp",
     price: 170.0,
     colors: [],
     offer: 0.15,
@@ -52,7 +57,8 @@ export const productsData = [
   {
     id: "6",
     title: "Modern Black Chair",
-    image: "/images/willdelete2/product-14-13_788x101355.png",
+    image:
+      "https://res.cloudinary.com/df1oiiaat/image/upload/v1751751986/product-14-13_788x101355_ibtjk6.webp",
     price: 100.0,
     colors: ["#000000", "#DDDDDD"],
     offer: 0.44,
@@ -62,7 +68,8 @@ export const productsData = [
   {
     id: "7",
     title: "Black Table Lamp",
-    image: "/images/willdelete2/product-14-13_788x101344.png",
+    image:
+      "https://res.cloudinary.com/df1oiiaat/image/upload/v1751751985/product-14-13_788x101344_dsuhyj.webp",
     price: 140.0,
     colors: ["#000000", "#FFFF00", "#FFFFFF"],
     offer: 0,
@@ -72,7 +79,8 @@ export const productsData = [
   {
     id: "8",
     title: "Stylish Travel Backpack",
-    image: "/images/willdelete2/product-14-13_788x101333.png",
+    image:
+      "https://res.cloudinary.com/df1oiiaat/image/upload/v1751751985/product-14-13_788x101333_dqcq9p.webp",
     price: 130.0,
     colors: ["#0000FF", "#000000", "#888888", "#0000FF", "#000000", "#888888"],
     offer: 0,
@@ -83,7 +91,7 @@ export const productsData = [
     id: "9",
     title: "Minimalist Wall Clock",
     image:
-      "/images/willdelete2/11_63e20b3b-21f2-49ff-93f8-2179551f43e0_700x900.png",
+      "https://res.cloudinary.com/df1oiiaat/image/upload/v1751751981/11_63e20b3b-21f2-49ff-93f8-2179551f43e0_700x900_qkoyxk.webp",
     price: 90.0,
     colors: ["#FFFFFF", "#000000", "#D3D3D3"],
     offer: 0.1,
@@ -94,7 +102,7 @@ export const productsData = [
     id: "10",
     title: "Yellow Decorative Pillow",
     image:
-      "/images/willdelete2/13_1c04d432-a1ba-4e9b-88a1-b9c3aecbab9d_700x900.png",
+      "https://res.cloudinary.com/df1oiiaat/image/upload/v1751751981/13_1c04d432-a1ba-4e9b-88a1-b9c3aecbab9d_700x900_denmpo.webp",
     price: 40.0,
     colors: ["#FFD700", "#FFA500"],
     offer: 0.05,
@@ -104,7 +112,8 @@ export const productsData = [
   {
     id: "11",
     title: "Modern Hanging Bulb",
-    image: "/images/willdelete2/13-1_788x1000.png",
+    image:
+      "https://res.cloudinary.com/df1oiiaat/image/upload/v1751751981/13-1_788x1000_efopt9.webp",
     price: 50.0,
     colors: ["#000000", "#FFFFFF"],
     offer: 0,
@@ -114,7 +123,8 @@ export const productsData = [
   {
     id: "12",
     title: "Black Hanging Chair",
-    image: "/images/willdelete2/5_700x900.png",
+    image:
+      "https://res.cloudinary.com/df1oiiaat/image/upload/v1751751980/5_700x900_aujb2h.webp",
     price: 310.0,
     colors: ["#000000"],
     offer: 0.12,
